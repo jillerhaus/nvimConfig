2ef8a6b89a865a870ae668ea47def70c7c435272
Johannes Illerhaus <johannes.illerhaus@ipp.mpg.de>
2024-05-12 17:43:28 +0200

    debugger

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,24 @@
 require "nvchad.mappings"
 
 -- add yours here
+ --local M = {}
+ --M.dap = {
+ --  plugin = true,
+ --  n = {
+ --    ["<leader>db"] = {
+ --      "<cmd> DapToggleBreakpoint <CR>",
+ --      "Add breakpoint at line",
+ --    },
+ --    ["<leader>dr"] = {
+ --      "<cmd> DapContinue <CR>",
+ --      "Start or continue the debugger",
+ --    }
+ --  }
+ --}
+ --return M
+local dapMappings = vim.keymap.set
+dapMappings("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", {desc = "Add breakpoint at line"} )
+dapMappings("n", "<leader>dr", "<cmd> DapContinue <CR>", {desc = "Start or continue the debugger"})
 
 local map = vim.keymap.set
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,46 @@
 return {
   {
+    "nvim-neotest/nvim-nio",
+    event = "VeryLazy",
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies =
+      {
+        "mfussenegger/nvim-dap",
+        "nvim-neotest/nvim-nio"
+      },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function ()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function ()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {}
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
     "jose-elias-alvarez/null-ls.nvim",
     event = "VeryLazy",
     opts = function()
@@ -28,7 +69,8 @@ return {
   	opts = {
   		ensure_installed = {
   			"lua-language-server", "stylua",
-  		  "prettier", "clangd", "clang-format"
+  		  "prettier", "clangd", "clang-format",
+        "codelldb",
   		},
   	},
   },
