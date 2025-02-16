c08d5bd67f4f0e39a5f4ff79ec84f76e40f531e2
Johannes Illerhaus <johannes.illerhaus@ipp.mpg.de>
2025-02-16 14:02:51 +0100

    added pyright config

--- init.lua
+++ init.lua
@@ -625,7 +625,13 @@ require("lazy").setup({
 			local servers = {
 				clangd = {},
 				-- gopls = {},
-				pyright = {},
+				pyright = {
+					settings = {
+						python = {
+							analysis = { diagnosticMode = "off", typeCheckingMode = "off" },
+						},
+					},
+				},
 				dockerls = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
